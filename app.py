8aeefae19cbfc58d7a43290c3c69ae62881999db
raaasin <nisarvskp@gmail.com>
2023-10-23 17:59:56 +0530

    trying out portforwarding

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-#first commit
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/hello', methods=['GET'])
+def hello():
+    return jsonify(message='Hello from Flask API!')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
